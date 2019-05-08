909037049bd09f14c8af9c856fb527185e182bc2
caflo <carl.flodin@hotmail.com>
2019-05-08 10:12:43 +0200

    cleanup

--- src/main.py
+++ src/main.py
@@ -10,13 +10,12 @@ def main():
     menu.menuLoop()
 
 
-def linkMaker():
+def kringla_link_maker(item_id='http://kulturarvsdata.se/raa/kmb/16000200041456'):  # Exempel id
     # Denna metod gör om ett itemID till en fungerade kringla länk
 
     base = 'http://www.kringla.nu/kringla/objekt?referens='  # Bas url till Kringla
-    itemID = 'http://kulturarvsdata.se/raa/kmb/16000200041456'  # Exempel id
 
-    url = itemID.split('.se/', 1)[1]  # Splittar på .se/ och lämnar kvar 'raa/kmb/16000200041456'
+    url = item_id.split('.se/', 1)[1]  # Splittar på .se/ och lämnar kvar 'raa/kmb/16000200041456'
     result = base + url  # Skapar en fungerade URL till kringla
 
     print(result)
@@ -25,5 +24,3 @@ def linkMaker():
 if __name__ == "__main__":
     # linkMaker()
     main()
-
-
